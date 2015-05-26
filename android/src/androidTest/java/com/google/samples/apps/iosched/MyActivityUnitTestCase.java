da3e45dec3a1729f23cfd910f442683a041502bc
MattDupree <kmdupr33@gmail.com>
2015-05-25 20:57:23 -0400

    added slides for talk

---
+++ android/src/androidTest/java/com/google/samples/apps/iosched/MyActivityUnitTestCase.java
@@ -0,0 +1,19 @@
+package com.google.samples.apps.iosched;
+
+import android.test.ActivityUnitTestCase;
+
+import com.google.samples.apps.iosched.ui.sessiondetail.SessionDetailActivity;
+
+/**
+ * Created by MattDupree on 5/25/15.
+ */
+public class MyActivityUnitTestCase extends ActivityUnitTestCase<SessionDetailActivity>{
+
+    public MyActivityUnitTestCase() {
+        super(SessionDetailActivity.class);
+    }
+
+    public void testShouldAddSessionToCalendar() {
+
+    }
+}
